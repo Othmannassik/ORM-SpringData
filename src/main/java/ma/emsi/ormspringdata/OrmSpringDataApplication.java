e54ea420b85bc5ada4e39df5c304e162417dc29f
Othman Nassik <othmannassik1@gmail.com>
2023-03-30 23:45:32 +0000

    Project Setup

---
+++ src/main/java/ma/emsi/ormspringdata/OrmSpringDataApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.ormspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmSpringDataApplication.class, args);
+    }
+
+}
