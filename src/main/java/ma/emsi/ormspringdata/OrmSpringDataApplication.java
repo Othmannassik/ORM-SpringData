a45864e1c8878073ba5cf667ec74c5ae32fb372a
Othman Nassik <othmannassik1@gmail.com>
2023-03-31 00:10:29 +0000

    Creation of Patient Entity & Patient Repository And fill the databse with elements

--- src/main/java/ma/emsi/ormspringdata/OrmSpringDataApplication.java
+++ src/main/java/ma/emsi/ormspringdata/OrmSpringDataApplication.java
@@ -1,13 +1,25 @@
 package ma.emsi.ormspringdata;
 
+import ma.emsi.ormspringdata.entities.Patient;
+import ma.emsi.ormspringdata.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrmSpringDataApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class OrmSpringDataApplication implements CommandLineRunner {
+    private PatientRepository patient;
     public static void main(String[] args) {
         SpringApplication.run(OrmSpringDataApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        patient.save(new Patient(null,"othman",new Date(), true, 198));
+        patient.save(new Patient(null,"aya",new Date(), false, 56));
+        patient.save(new Patient(null,"akram",new Date(), false, 87));
+        patient.save(new Patient(null,"hatim",new Date(), false, 29));
+    }
 }
